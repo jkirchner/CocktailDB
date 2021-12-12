678ae97a86d3c0824570eeb44f76c1fb005e4e89
Jason Kirchner <jason.kirchner@me.com>
2021-12-12 00:19:35 -0700

    Initial Commit

---
+++ CocktailDB/CocktailDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailDBApp.swift
+//  CocktailDB
+//
+//  Created by Jason Kirchner on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CocktailDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
